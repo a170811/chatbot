4c74be3507acc007a59c58871f1730d30df83a34
a170811 <a170811@gmail.com>
2019-06-11 20:36:24 +0800

    refine preprocessing

--- model/preprocessing.py
+++ model/preprocessing.py
@@ -18,36 +18,43 @@ def load_txt(file_name):
         contents = np.array(contents)
     return contents
 
-# load data
-wb = load_workbook('./data/QA_12543.xlsx')
-data = [[str(row[0].value), str(row[1].value)]for row in wb['工作表1'].rows]
+def main():
 
-place = load_txt('place.txt')
-store = load_txt('store.txt')
-food = load_txt('food.txt')
+    # load data
+    wb = load_workbook('./data/QA_12543.xlsx')
+    data = [[str(row[0].value), str(row[1].value)]for row in wb['工作表1'].rows]
 
-data_t = [[remove_punc(content[0]), remove_punc(content[1])] for content in data]
+    place = load_txt('place.txt')
+    store = load_txt('store.txt')
+    food = load_txt('food.txt')
 
+    data_t = [[remove_punc(content[0]), remove_punc(content[1])] for content in data]
 
-jieba.load_userdict('./data/dict_tw.txt')
-jieba.load_userdict('./data/food.txt')
-jieba.load_userdict('./data/place.txt')
-jieba.load_userdict('./data/store.txt')
-jieba.load_userdict('./data/other.txt')
+    print('loading')
 
-#transform data
-cut_q_res = []
-cut_a_res = []
-for QA in data_t:
-    cut_q_res.append(list(jieba.cut(QA[0])))
-    cut_a_res.append(list(jieba.cut(QA[1])))
+    jieba.load_userdict('./data/dict_tw.txt')
+    jieba.load_userdict('./data/food.txt')
+    jieba.load_userdict('./data/place.txt')
+    jieba.load_userdict('./data/store.txt')
+    jieba.load_userdict('./data/other.txt')
 
-q_res, a_res = zip(*data)
+    #transform data
+    cut_q_res = []
+    cut_a_res = []
+    for QA in data_t:
+        cut_q_res.append(list(jieba.cut(QA[0])))
+        cut_a_res.append(list(jieba.cut(QA[1])))
 
-cut_qa_combine = np.array(cut_q_res) + np.array(cut_a_res)
-pickle.dump(data, open('./data/qa.pkl', 'wb'))
-pickle.dump(q_res, open('./data/question.pkl', 'wb'))
-pickle.dump(a_res, open('./data/answer.pkl', 'wb'))
-pickle.dump(cut_q_res, open('./data/cut_question.pkl', 'wb'))
-pickle.dump(cut_a_res, open('./data/cut_answer.pkl', 'wb'))
-pickle.dump(cut_qa_combine, open('./data/cut_qa_combine.pkl', 'wb'))
+    q_res, a_res = zip(*data)
+
+    cut_qa_combine = np.array(cut_q_res) + np.array(cut_a_res)
+    pickle.dump(data, open('./data/qa.pkl', 'wb'))
+    pickle.dump(q_res, open('./data/question.pkl', 'wb'))
+    pickle.dump(a_res, open('./data/answer.pkl', 'wb'))
+    pickle.dump(cut_q_res, open('./data/cut_question.pkl', 'wb'))
+    pickle.dump(cut_a_res, open('./data/cut_answer.pkl', 'wb'))
+    pickle.dump(cut_qa_combine, open('./data/cut_qa_combine.pkl', 'wb'))
+
+if '__main__' == __name__:
+
+    main()
